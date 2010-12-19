62dc5f79a968780a414f4a010bbdfb9c0f10a7cd
Martin Storsjo <martin@martin.st>
2010-12-19 17:07:26 +0200

    64-bit safe pointer alignment

--- aacenc/src/memalign.c
+++ aacenc/src/memalign.c
@@ -23,6 +23,7 @@
 
 
 #include	"memalign.h"
+#include	<stdint.h>
 
 /*****************************************************************************
 *
@@ -66,8 +67,8 @@ mem_malloc(VO_MEM_OPERATOR *pMemop, unsigned int size, unsigned char alignment, 
 		pMemop->Set(CodecID, tmp, 0, size + alignment);
 
 		mem_ptr =
-			(unsigned char *) ((unsigned int) (tmp + alignment - 1) &
-					(~((unsigned int) (alignment - 1))));
+			(unsigned char *) ((intptr_t) (tmp + alignment - 1) &
+					(~((intptr_t) (alignment - 1))));
 
 		if (mem_ptr == tmp)
 			mem_ptr += alignment;
