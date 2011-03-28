0aacde1d75117e9cce1083354c2ecb00acc38702
Martin Storsjo <martin@martin.st>
2011-03-28 23:18:57 +0200

    Convert the test application to pure C

--- aac-enc.c
+++ aac-enc.c
@@ -31,6 +31,18 @@ void usage(const char* name) {
 int main(int argc, char *argv[]) {
 	int bitrate = 64000;
 	int ch;
+	const char *infile, *outfile;
+	FILE *out;
+	void *wav;
+	int format, sampleRate, channels, bitsPerSample;
+	int inputSize;
+	uint8_t* inputBuf;
+	int16_t* convertBuf;
+	VO_AUDIO_CODECAPI codec_api = { 0 };
+	VO_HANDLE handle = 0;
+	VO_MEM_OPERATOR mem_operator = { 0 };
+	VO_CODEC_INIT_USERDATA user_data;
+	AACENC_PARAM params = { 0 };
 	while ((ch = getopt(argc, argv, "r:")) != -1) {
 		switch (ch) {
 		case 'r':
@@ -46,15 +58,14 @@ int main(int argc, char *argv[]) {
 		usage(argv[0]);
 		return 1;
 	}
-	const char* infile = argv[optind];
-	const char* outfile = argv[optind + 1];
+	infile = argv[optind];
+	outfile = argv[optind + 1];
 
-	void* wav = wav_read_open(infile);
+	wav = wav_read_open(infile);
 	if (!wav) {
 		fprintf(stderr, "Unable to open wav file %s\n", infile);
 		return 1;
 	}
-	int format, sampleRate, channels, bitsPerSample;
 	if (!wav_get_header(wav, &format, &channels, &sampleRate, &bitsPerSample, NULL)) {
 		fprintf(stderr, "Bad wav file %s\n", infile);
 		return 1;
@@ -67,14 +78,10 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Unsupported WAV sample depth %d\n", bitsPerSample);
 		return 1;
 	}
-	int inputSize = channels*2*1024;
-	uint8_t* inputBuf = new uint8_t[inputSize];
-	int16_t* convertBuf = new int16_t[inputSize/2];
+	inputSize = channels*2*1024;
+	inputBuf = (uint8_t*) malloc(inputSize);
+	convertBuf = (int16_t*) malloc(inputSize);
 
-	VO_AUDIO_CODECAPI codec_api = { 0 };
-	VO_HANDLE handle = 0;
-	VO_MEM_OPERATOR mem_operator = { 0 };
-	VO_CODEC_INIT_USERDATA user_data;
 	voGetAACEncAPI(&codec_api);
 
 	mem_operator.Alloc = cmnMemAlloc;
@@ -86,7 +93,6 @@ int main(int argc, char *argv[]) {
 	user_data.memData = &mem_operator;
 	codec_api.Init(&handle, VO_AUDIO_CodingAAC, &user_data);
 
-	AACENC_PARAM params = { 0 };
 	params.sampleRate = sampleRate;
 	params.bitRate = bitrate;
 	params.nChannels = channels;
@@ -96,20 +102,22 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	FILE* out = fopen(outfile, "wb");
+	out = fopen(outfile, "wb");
 	if (!out) {
 		perror(outfile);
 		return 1;
 	}
 
-	while (true) {
+	while (1) {
 		VO_CODECBUFFER input = { 0 }, output = { 0 };
 		VO_AUDIO_OUTPUTINFO output_info = { 0 };
+		int read, i;
+		uint8_t outbuf[20480];
 
-		int read = wav_read_data(wav, inputBuf, inputSize);
+		read = wav_read_data(wav, inputBuf, inputSize);
 		if (read < inputSize)
 			break;
-		for (int i = 0; i < read/2; i++) {
+		for (i = 0; i < read/2; i++) {
 			const uint8_t* in = &inputBuf[2*i];
 			convertBuf[i] = in[0] | (in[1] << 8);
 		}
@@ -117,7 +125,6 @@ int main(int argc, char *argv[]) {
 		input.Length = read;
 		codec_api.SetInputData(handle, &input);
 
-		uint8_t outbuf[20480];
 		output.Buffer = outbuf;
 		output.Length = sizeof(outbuf);
 		if (codec_api.GetOutputData(handle, &output, &output_info) != VO_ERR_NONE) {
@@ -126,8 +133,8 @@ int main(int argc, char *argv[]) {
 		}
 		fwrite(outbuf, 1, output.Length, out);
 	}
-	delete [] inputBuf;
-	delete [] convertBuf;
+	free(inputBuf);
+	free(convertBuf);
 	fclose(out);
 	codec_api.Uninit(handle);
 	wav_read_close(wav);
